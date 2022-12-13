1923c15291468aa74435394ba1749e0f17349f74
Андрей Грушевский <gruand69@yandex.ru>
2022-12-13 12:10:47 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
 
-def attack(char_name, char_class):
+
+def attack(char_name: str, char_class: str) -> str:
+    """Иммитирует атаку."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}'
                 )
@@ -15,7 +18,8 @@ def attack(char_name, char_class):
             f'{char_name} не нананёс урон противнику')
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
+    """Иммитирует защиту."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,7 +29,8 @@ def defence(char_name, char_class):
     return (f'{char_name} не блокировал урона')
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
+    """Специальная функция."""
     if char_class == 'warrior':
         return (
             f'{char_name} применил специальное умение «Выносливость {80 + 25}»'
@@ -37,7 +42,8 @@ def special(char_name, char_class):
     return (f'{char_name} не применил специальное')
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
+    """Тренировка."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -46,8 +52,8 @@ def start_training(char_name, char_class):
         print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
     print('Потренируйся управлять своими навыками.')
     print('Введи одну из команд: attack — чтобы атаковать противника, defence '
-          + '— чтобы блокировать атаку противника или special —'
-          + ' чтобы использовать свою суперсилу.')
+          '— чтобы блокировать атаку противника или special —'
+          ' чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
     cmd = None
     while cmd != 'skip':
@@ -62,28 +68,30 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Выбор персонажа."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого '
-                           + 'хочешь играть: Воитель — warrior, '
-                           + 'Маг — mage, Лекарь — healer: ')
+                           'хочешь играть: Воитель — warrior, '
+                           'Маг — mage, Лекарь — healer: ')
         if char_class == 'warrior':
             print('Воитель — дерзкий воин ближнего боя. '
-                  + 'Сильный, выносливый и отважный.')
+                  'Сильный, выносливый и отважный.')
         if char_class == 'mage':
             print('Маг — находчивый воин дальнего боя. '
-                  + 'Обладает высоким интеллектом.')
+                  'Обладает высоким интеллектом.')
         if char_class == 'healer':
             print('Лекарь — могущественный заклинатель. '
-                  + 'Черпает силы из природы, веры и духов.')
+                  'Черпает силы из природы, веры и духов.')
         approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
-                               + 'или любую другую кнопку, чтобы выбрать '
-                               + 'другого персонажа ').lower()
+                               'или любую другую кнопку, чтобы выбрать '
+                               'другого персонажа ').lower()
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
@@ -93,6 +101,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
